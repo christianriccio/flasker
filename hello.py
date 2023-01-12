e4e4ac7bf1c1412887c320b686c40262f7a5bdbf
Christian Riccio <riccio.christian.21@gmail.com>
2023-01-12 15:28:50 +0100

    initial commit

---
+++ hello.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+#route decorator for homepage
+@app.route('/')
+def index():
+    pizza = 'margherita'
+    return render_template('index.html', pizza = pizza)
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name = name)
+
+# custom error page 
+# invalid url
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+# internal server error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
+
+
+
